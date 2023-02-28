f3657858c5e87607f50ad75fe45b98fada789ef1
Łukasz Śmigiel <81709506+L-Smigiel@users.noreply.github.com>
2023-02-28 16:05:01 +0100

    it works!

--- HEADER-OVERLAY ONSCROLL.js
+++ HEADER-OVERLAY ONSCROLL.js
@@ -1,17 +1,16 @@
 function myFunction() {
-  const headerElement = document.getElementById("site-header")
+  const headerElement = document.getElementById("site-header") 
   const headerOverlay = headerElement.appendChild(document.createElement('div'));
   headerOverlay.classList.add("header-overlay");
 }
 myFunction()
 
-const headerOverlay = document.querySelector(".header-overlay")
-const bodyGrab = document.querySelector("body");
-bodyGrab.addEventListener("onscroll", setOpacityIndicator());
 
-function setOpacityIndicator() {
-headerOverlay.style.opacity =+ 0.01;
+window.onscroll = () => {
+  const headerOverlay = document.querySelector(".header-overlay")
+  if (window.scrollY * 0.1 < 100){
+    headerOverlay.style.opacity = window.scrollY * 0.1;
+  } else {
+    headerOverlay.style.opacity = 100;
+  }
 }
-
-//to nie działa
-//użyj tego stary: https://developer.mozilla.org/en-US/docs/Web/API/Window/scroll
